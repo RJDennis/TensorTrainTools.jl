0250ccb126accc24090b329d6dc8c6b8c294c9c5
Documenter.jl <documenter@juliadocs.github.io>
2024-01-30 20:13:23 +0000

    build based on 96b3dbd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TensorTrainApprox","category":"page"},{"location":"#TensorTrainApprox","page":"Home","title":"TensorTrainApprox","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TensorTrainApprox.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TensorTrainApprox]","category":"page"}]
+}
